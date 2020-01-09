ee9e858fd0d7e8e5a143f4db0fc2846cbe045198
= <=>
2020-01-09 08:43:29 +0000

    MonthCalendar finalizado

--- src/main/java/dad/javafx/componentes/MonthCalendar.java
+++ src/main/java/dad/javafx/componentes/MonthCalendar.java
@@ -10,7 +10,6 @@ import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
-import java.time.Month;
 import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -82,6 +81,8 @@ public class MonthCalendar extends VBox implements Initializable {
 	 */
 	private void onYearChanged(Number nv) {
 		
+		// Cuando cambia el año, debemos cambiar todo el componente
+		// para actualizarlo con los datos nuevos
 		for( int i = 1; i < mainGrid.getChildren().size(); i++ ) {
 			mainGrid.getChildren().removeAll(daysList.get());
 		}
@@ -102,15 +103,20 @@ public class MonthCalendar extends VBox implements Initializable {
 		
 		int year = yearProperty.get();
 		
+		// Actualizamos en el mes que estamos, ponemos el nombre
 		monthTxt.setText( months[nv.intValue()-1]);
 		
+		// Calendario
 		Calendar calendar = Calendar.getInstance();
 		
+		// Ponemos la fecha del calendario
 		calendar.set(year, nv.intValue()-1, 0);
 		int dayInit = calendar.get(Calendar.DAY_OF_WEEK);
 		
+		
 		int d = 1;
 		int row = 1;
+		// Número de meses
 		int totalDays = YearMonth.of(getYearProperty(), nv.intValue()).lengthOfMonth();
 		while( d <= totalDays ) {
 			
@@ -118,6 +124,8 @@ public class MonthCalendar extends VBox implements Initializable {
 			
 			for ( i = dayInit ; i <= 7 && d <= totalDays; i++) {
 
+				// Ponemos los dias que tiene la semana, empezando siempre
+				// por el dia 1 correspondiente al mes
 				Label day = new Label(String.valueOf(d));
 				daysList.add(day);
 				day.setPrefHeight(USE_COMPUTED_SIZE);
@@ -127,6 +135,7 @@ public class MonthCalendar extends VBox implements Initializable {
 			
 			dayInit = 1; // Volvemos a ponerlo desde el principio
 			
+			// Actualzamos las filas para que se ajustan a lo que queremos 
 			RowConstraints r = new RowConstraints(USE_COMPUTED_SIZE, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE);
 			r.setFillHeight(true);
 			r.setVgrow(Priority.ALWAYS);
